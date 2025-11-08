1da4cc40bc34ca3b5b16bcad5092dffb1d7b2f46
timjschowalter-lgtm <timjschowalter@gmail.com>
2025-11-08 17:50:10 -0500

    Removed unused dependencies

--- predict.py
+++ predict.py
@@ -1,10 +1,4 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
-import numpy as np
-import uvicorn
 from fastapi import FastAPI
 import pickle
 app = FastAPI()
--- train.py
+++ train.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
-import numpy as np
 
 def main():
     """
