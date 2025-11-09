10a7451c88c3915e1df359a6a0868b14ee10af88
Peter Hart <peterjhart1010@gmail.com>
2025-11-08 19:58:09 -0500

    crucial guard statement

--- frontend/hack-frontend/src/App.tsx
+++ frontend/hack-frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   
   const [email,setEmail] = useState("");
   useEffect(()=>{
+    if(!email){return};
     fetch("http://localhost:8000/predict_phishing", {
       method: 'POST',
       headers: {
